34ed04522a9bc64a244286c0da748e4e0eeda194
Neelotpal Dey <52033764+Neelotpal-Dey@users.noreply.github.com>
2024-12-22 20:04:15 +0530

    Add files via upload

---
+++ protfolio.py
@@ -0,0 +1,93 @@
+import streamlit  as st
+st.title("Neelotpal Dey")
+st.caption("The Art Of Living Faculty | Assistant Professor CS/IT")
+st.write("Varanasi, Uttar Pradesh, India")
+t1,t3,t2,t4,t5 = st.tabs(['About Me','Education', 'Experience', 'Certification',
+                       'Project | Research Word'])
+c1,c2 =t1.columns([6,2])
+c1.text('''Teaching Experience: 8 yrs+
+Industrial Trainer Experience: 4 yrs+
+Trainer for C, Cpp, Java, C#, Python, JavaScript, PHP, MySQL, Visual Basic, Networking, HTML and CSS 
+Motivational Speaker, Podcast and Hosting Experience: 3 yrs+ 
+Art Of Living Faculty for 2 yrs+ 
+Sri Sri Sanskar Kendra Faculty for 3 yrs+ 
+Sri Sri Sanskar Kendra Varanasi District Coordinator 
+Counciling Experience: 3 yrs+ 
+Singer, Coder, Guitarist, Poet, Lyricist, Sadhak, Observer, Seeker, Listener
+Ex St.Johnian, Ex SMSian, IGNOU
+BCA, MCA, MSc. CFT''')
+c2.image("me.gif")
+
+t2.write(":red[**Microtek College of Management & Technology Varanasi & Jaunpur** : 9 months]")
+t2.caption("Sankat Mochan,Varanasi, Uttar Pradesh, India")
+t2.text('''Training and Placement Cell Incharge:   July 2024 - Present (6 months)
+Trainer Faculty for ML, DS, Generative AI, Advanced Excel and Data Visualization Internship Programs Under Technology Incubation Hub(TIH) IIT Patna
+
+Assistant Professor:    April 2024 - Present (9 months)
+Assistant Professor for BCA, MBA, MCA, PGDCA affiliated to MGKVP, VBSPU, AKTU, AUS
+
+SPOC for SIH 2024:  September 2024 - November 2024 (3 months)
+Single Person Of Contact and Mentor for Smart India Hackathon 2024''')
+t2.divider()
+
+t2.write(":red[**The Art of Living** : 8 years 7 months]")
+t2.write("Teacher : March 2022 - Present (2 years 10 months)")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.text('''Happiness Program (HP)  |  Happiness Program for Youth (HPY)  |  Prison Smart
+Online Breath and Meditation Workshop (OBMW)  |  Rural Happiness Program
+Youth Leadership and Training Program (YLTP)  |  Know Your Child (KYC)  |  Know Your Teen (KYT)''')
+t2.write("Volunteer : June 2016 - Present (8 years 7 months)")
+t2.caption("Harsh Hospital, Bhelupura, Varanasi")
+t2.write("Teacher Assistant : September 2020 - March 2022 (1 year 7 months)")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.divider()
+
+t2.write(":red[**Sri Sri Tattva** : 2 years 10 months]")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.text(''' Affiliate Marketing : March 2022 - Present (2 years 10 months)
+100% Authentic Ayurvedic Medicine and Organic Food Products
+use code "ND154" for additional discount mentioned on Website.''')
+t2.divider()
+
+t2.write(":red[**Sri Sri School of Yoga** : 2 months]")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.text('''Assistant Yoga Instructor : August 2022 - September 2022 (2 months)
+50 Hours of Practical and Theoretical training of Yoga and Pranayama
+Certificate Number: 0FN4263/SYP/007''')
+t2.divider()
+
+t2.write(":red[**Sri Sri Sanskar Kendra** : 4 years]")
+t2.write("Teacher : January 2021 - Present (4 years)")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.write("District Coordinator: February 2022 - Present (3 years)")
+t2.caption("Varanasi District, Uttar Pradesh, India")
+t2.divider()
+
+t2.write(":red[Coding Café]")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.write("Owner : April 2020 - Present (4 years 9 months)")
+t2.text('''Interviewer : May 2021 - Present (3 years 8 months)
+Training and Conducting Mock Interview and Freelance Interviewer for several companies.''')
+t2.text('''Freelance | Industrial Trainer : April 2019 - Present (5 years 9 months)
+"C, Java, Python, Web Designing, Photo and Video Editing, Networking''')
+t2.text('''Freelance | Private Tutor : April 2016 - Present (8 years 9 months)"
+C, C++, Java, Python, Networking, MS Office''')
+t2.divider()
+
+t2.write(":red[Webtek Computer Education]")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.text('''Python Trainer : February 2022 - May 2024 (2 years 4 months)
+Trainer for Python Programing, C, CPP, Core Java to UG, PG and 8-12 students.''')
+t2.divider()
+
+t2.write(":red[EISystems Technologies]")
+t2.text('''Online Internship : January 2023 - January 2023 (1 month)
+Data Science Training with Project on Mobile Price Prediction Model using Python Libraries and Streamlit
+Ref. ID: 22ESYSN0693''')
+t2.text('''Winter Internship : November 2021 - December 2021 (2 months)
+Android Application Development
+Reference ID: 21ESYSN946''')
+t2.write("Summer Internship : May 2019 - July 2019 (3 months)")
+t2.caption("Varanasi, Uttar Pradesh, India")
+t2.text('''Data Analytics and Machine Learning
+Reference ID: 19SDATVNS030''')
